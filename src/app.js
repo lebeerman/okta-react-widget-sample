7058d537cd0dee31dee5732714bc43f3a76705ae
lebeerman <omnidbeerman@gmail.com>
2018-02-19 22:52:18 -0700

    Made changes following Lee's tutorial on Okta's site. Got it mostly going then made some changes.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
+import LoginPage from './components/auth/LoginPage';
 import Layout from './components/common/Layout';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
@@ -11,10 +11,11 @@ import '../scss/site.scss';
 render(
   <Router history={browserHistory}>
     <Route path="/" component={Layout}>
-      <IndexRoute component={HomePage}/>
-      <Route path="/about" component={AboutPage}/>
-      <Route path="/contact" component={ContactPage}/>
+      <IndexRoute component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/contact" component={ContactPage} />
+      <Route path="/login" component={LoginPage} />
     </Route>
   </Router>,
   document.getElementById('app')
-)
+);
---
+++ src/components/auth/LoginPage.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import OktaSignIn from '@okta/okta-signin-widget';
+
+export default class LoginPage extends React.Component{
+  constructor(){
+    super();
+    this.state = { user: null };
+    this.widget = new OktaSignIn({
+      baseUrl: 'https://dev-375655.oktapreview.com',
+      clientId: '0oae2nm0diDWGUMxM0h7',
+      redirectUri: 'http://localhost:8080',
+      authParams: {
+        responseType: 'id_token'
+      }
+    });
+
+    this.showLogin = this.showLogin.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount(){
+    this.widget.session.get((response) => {
+      if(response.status !== 'INACTIVE'){
+        this.setState({user:response.login});
+      }else{
+        this.showLogin();
+      }
+    });
+  }
+
+  showLogin(){
+    Backbone.history.stop(); 
+    //utilizes Backbone.js to navigate its own screens - once rendered, it starts the backbone history. 
+    // It rerenders whenever the function is called, so this forces it to stop.
+    this.widget.renderEl({el:this.loginContainer},
+      (response) => {
+        this.setState({user: response.claims.email});
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  logout(){
+    this.widget.signOut(() => {
+      this.setState({user: null});
+      this.showLogin();
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.user ? (
+          <div className="container">
+            <div>Welcome, {this.state.user}!</div>
+            <button onClick={this.logout}>Logout</button>
+          </div>
+        ) : null}
+        {this.state.user ? null : (
+          <div ref={(div) => {this.loginContainer = div; }} />
+        )}
+      </div>
+    );
+  }
+}
